3e28e4aef78ca3d7b4c241dfc870521225e2a676
C B Dev Narayan <devcb@pg.cusat.ac.in>
2022-05-20 16:01:50 +0530

    Update Reverse.java

--- Lab Practice/Reverse.java
+++ Lab Practice/Reverse.java
@@ -9,14 +9,13 @@ class Reverse
         int Nt = N;
         int Rev = 0;
         System.out.print("Reverse : ");
+        int C = 0;
         while (Nt != 0)
         {
+            System.out.print(Nt%10);
             Rev = (Rev*10) + (Nt%10);
-            if (Nt%10==0){
-                System.out.print("0");
-            }
             Nt = Nt / 10;
         }
-        System.out.println(Rev);
+        //System.out.println(Rev);
     }
 }
