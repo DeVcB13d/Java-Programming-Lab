7389824b525d6d404ad4fc4af407812fcfeb35ba
C B Dev Narayan <devcb@pg.cusat.ac.in>
2022-05-27 14:55:48 +0530

    add more p1

--- Lab Practice/Reverse.java
+++ Lab Practice/Reverse.java
@@ -9,13 +9,11 @@ class Reverse
         int Nt = N;
         int Rev = 0;
         System.out.print("Reverse : ");
-        int C = 0;
         while (Nt != 0)
         {
             System.out.print(Nt%10);
             Rev = (Rev*10) + (Nt%10);
             Nt = Nt / 10;
         }
-        //System.out.println(Rev);
     }
 }
---
+++ src/shape.java
@@ -0,0 +1,14 @@
+import java.util.*
+
+public abstract class shape {
+	public static void computeArea(int a,int b) {
+		return a*b;
+	}
+	public static void computeArea(int a){
+		return a*a;
+	}
+	public static void computeArea(float r) {
+		return 3.14*r*r;
+	}
+	public static void 
+}
